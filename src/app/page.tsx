df7cc436e48aff4f688dad550ffb354436ce7d92
Yohan Yang <yangyohan1@gmail.com>
2023-05-16 10:57:36 -0400

    added a tag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image';
 
 
 export default function Home() {
@@ -6,6 +5,7 @@ export default function Home() {
     <div className='flex justify-center'>
     <h1 className='text-center'> MKKY </h1>
     <button className="btn btn-primary mt-10">Button</button>
+    <a href='/components/home'>GO HOME</a>
     </div>
   )
 }
