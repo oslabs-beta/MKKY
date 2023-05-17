949c7e69188816fa749ef8d0a6f187adb7901098
Kelvin Xu <kelvinxu1008@gmail.com>
2023-05-17 19:42:16 -0400

    after signing in now directs to the home page and home page has sign up button

--- src/app/components/home/page.tsx
+++ src/app/components/home/page.tsx
@@ -1,13 +1,14 @@
 //when there is layout.tsx, next.js goes to layout to render rather than just the page
-// import React from 'react'; <-- not needed??
-// import NextAuth from "next-auth/next";
-// import Provider from 'next-auth/providers' //OAuth
+
+'use client'
+import {signOut} from 'next-auth/react';
 
 export default function Something(){
     return (
         <div className='flex justify-center'>
         <h1 className='text-center'> MKKY </h1>
         <h2 className="mt-10">FUCK YESSSSSS</h2>
+        <button onClick={() => signOut({ callbackUrl: 'http://localhost:3000'})}> SignOut</button>
         </div>
       )
 };
