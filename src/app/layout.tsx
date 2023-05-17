c6adeef136e9a107a39ae51ce279fedbc35fc3b9
Kelvin Xu <kelvinxu1008@gmail.com>
2023-05-17 17:53:08 -0400

    successful OAuth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Provider from './Providers';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,7 +16,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Provider>
+        {children}
+        </Provider>
+        </body>
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
-import Image from 'next/image';
+// import Image from 'next/image';
+import Link from 'next/link';
 
 
 export default function Home() {
   return (
     <div className='flex justify-center'>
-    <h1 className='text-center'> MKKY </h1>
-    <button className="btn btn-primary mt-10">Button</button>
+    <h1 className='text-center'>MKKY</h1>
+    <Link href='/components/login'
+    className='text-center mt-6'>
+    Sign In
+    </Link>
+    <Link href='/components/signUp'
+    className='text-center mt-10'>
+    Sign Up
+    </Link>
     </div>
   )
 }
