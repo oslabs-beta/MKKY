34eb39159ba4fc13e52d7c270a34647f4840220e
mushfiq-khandaker <127776658+mushfiq-khandaker@users.noreply.github.com>
2023-05-17 10:28:01 -0400

    working on visualizing data

---
+++ src/app/components/home/page.tsx
@@ -0,0 +1,18 @@
+import React from "react"
+//import ReactDOM from 'react-dom';
+import Display from "./display"
+
+function App() {
+    const uri = "postgres://mmethhdd:OuENml3Y4wNyMcCHb69l16Cn3l2osxzh@drona.db.elephantsql.com/mmethhdd";
+  
+    return (
+      <div>
+        <Display />
+      </div>
+    );
+  };
+  
+  export default App;
+
+
+
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
     <div className='flex justify-center'>
     <h1 className='text-center'> MKKY </h1>
     <button className="btn btn-primary mt-10">Button</button>
+    <Link href = "/components/home">GO TO HOME</Link>
     </div>
   )
 }
