2e58ff172387e738fb6cbbbd694a25e92f30ed10
kennyyang2015 <kennyyang2015@gmail.com>
2023-05-18 10:28:27 -0400

    finished welcome and partially navbar, merging with kelvin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
-import Image from 'next/image';
+import React from 'react';
 
-
-export default function Home() {
+const Welcome: React.FC = () => {
   return (
-    <div className='flex justify-center'>
-    <h1 className='text-center'> MKKY </h1>
-    <button className="btn btn-primary mt-10">Button</button>
+    <div className="flex flex-col items-center">
+      <p className="text-lg mt-5">MKKY PRESENTS: </p>
+      <h1 className="text-5xl font-bold">SQL Visual Interface</h1>
+      <p className="text-lg mt-5">Our tool is designed for developers to visualize a database</p>
     </div>
-  )
-}
+  );
+};
+
+export default Welcome;
