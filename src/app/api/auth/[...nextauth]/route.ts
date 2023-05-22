a3638d66c765c05600de597c3e290d27fd0fc42b
Kelvin Xu <kelvinxu1008@gmail.com>
2023-05-22 14:27:03 -0400

    auth successfully connected with mongoDB

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,21 +1,16 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from 'next-auth/providers/google' //OAuth
 import GitHubProvider from "next-auth/providers/github";
-import CredentialsProvider from "next-auth/providers/credentials";
-// export default NextAuth({
-//     providers: [
-//         GoogleProvider({
-//             //these inside .env with the correct label
-//             clientId: process.env.GOOGLE_ID!,
-//             clientSecret: process.env.GOOGLE_CLIENT_SECRET!
-//         })
-//     ],
-// });
+// import CredentialsProvider from "next-auth/providers/credentials";
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter"
+import clientPromise from "../lib/mongodb"
+// import { connectToDB } from '../../../utils/database';
+// import User from '../../../model/user';
 
 const handler = NextAuth({
+  adapter: MongoDBAdapter(clientPromise),
     providers: [
         GoogleProvider({
-            //these inside .env with the correct label
             clientId: process.env.GOOGLE_ID!,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
             authorization: {
@@ -42,7 +37,9 @@ const handler = NextAuth({
         //   }
         // })      //         STILL WORKING ON THIS TO ADD CLIENT BASE ON EMAIL AND PASSWORD WITHOUT OATH  DONT DELETE JUST YET `
     ],
-  })
+    //when working with handler variable, we need to use callbacks
+    
+})
   
   export {handler as GET, handler as POST};
   //this will overwrite my callbackUrl in login page.tsx
