a2056913d8bd6bde6b7aef39b6bdf6658790ae72
Kelvin Xu <kelvinxu1008@gmail.com>
2023-05-22 14:27:03 -0400

    auth successfully connected with mongoDB

--- src/app/utils/database.tsx
+++ src/app/utils/database.tsx
@@ -1,1 +1,27 @@
 import mongoose from 'mongoose';
+
+let isConnected = false; 
+
+export const connectToDB = async () => {
+  //only data specified in Schema will be passed to database
+  mongoose.set('strictQuery', true);
+
+  if(isConnected){
+    console.log('MongoDB is already connected');
+    return
+  }
+  try{
+    await mongoose.connect(process.env.MONGODB_URI!);
+    isConnected = true;
+    console.log('Mongoose is now connected');
+
+    mongoose.connection.once('open', function() {
+      console.log('Mongoose has opened a connection');
+    });
+  }
+  catch(error){
+    console.log(error);
+  }
+}
+
+//connectToDB();
