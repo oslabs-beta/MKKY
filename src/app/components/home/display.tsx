cbf5151be3090dde49fabb5e5c00178d43e5ea95
mushfiq-khandaker <127776658+mushfiq-khandaker@users.noreply.github.com>
2023-05-24 14:14:08 -0400

    Trying to Connect Client to database

--- src/app/components/home/display.tsx
+++ src/app/components/home/display.tsx
@@ -1,39 +1,45 @@
 //'use client'
 
-import React, { useState, useEffect } from "react";
+import React , {createContext} from "react";
 import Chart from "chart.js";
 import {Pool} from 'pg';
-import {Client} from '@elephantsql/client';
+//import { useServerEffect } from 'next/server';
 
 import TableCell from "./tableCell";
 
 import "./style.css"
 
+import {executeQuery} from './db'
+//const UpdateQueryContext = createContext(null)
 const Display = async (props) =>{
 
   let pg = require('pg')
-  //const URI = 'postgres://mmethhdd:OuENml3Y4wNyMcCHb69l16Cn3l2osxzh@drona.db.elephantsql.com/mmethhdd'
-  const URI = props.URI
-  let client = new pg.Client(URI)
-  client.connect()
-
-  const allTables = await client.query("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public';")
+  const URI = props.URI 
+  
+  //let client = new pg.Client(URI + '?cf_bypass=true')
+  // let client = new Pool({
+  //   connectionString:URI
+  // })
+  //client.connect() 
+  const allTables = await executeQuery("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public';")
+  //const allTables = await client.query("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public';")
   //allTables.rows.pop()
   const allTableNames = Object.values(allTables.rows)
-
-
   console.log('TABLE NAMES', allTableNames)
 
+  
   let allTablesData = []
   let tableData;
-
+  
   //Removing SQL default table
   allTableNames.pop()
   allTableNames.forEach( async (table) => {
-    tableData = await client.query(`SELECT * FROM ${table.table_name}`)
+    tableData = await executeQuery(`SELECT * FROM ${table.table_name}`)
+    //tableData = await client.query(`SELECT * FROM ${table.table_name}`)
     allTablesData.push(tableData)
   })
-  tableData = await client.query(`SELECT * FROM ${allTableNames[0].table_name}`);
+  tableData = await executeQuery(`SELECT * FROM ${allTableNames[0].table_name}`);
+  //tableData = await client.query(`SELECT * FROM ${allTableNames[0].table_name}`);
 
   allTables.rows.pop()
   
@@ -46,23 +52,21 @@ const Display = async (props) =>{
     })
     allTablesFields.push(newFieldsArr)
   })
+  
+  //await client.end()
 
-  function onSubmitQuery(tableID, rowID, columnID) {
-    return
-  }
-
-  //const [value, setValue] = useState("")
 
   return (
+    
     <div>
       {console.log("All DATA", allTablesData)}
       {console.log("FIELDS", allTablesData[0].fields)}
       {console.log(allTables.rows)}
       {console.log("ALL FIELDS", allTablesFields)}
 
-
       <style>{`td { border : 4px solid blue}`}</style>
         {allTables.rows.map((table:any, index: number) => (
+          
           <div>
             <h1>{table.table_name}</h1>
               
@@ -74,9 +78,9 @@ const Display = async (props) =>{
               {allTablesData[index].rows.map((row: any) => (
                 <tr key={row.id}>
                   {Object.keys(row).map((cell:any, colIndex: number) => (
-                    
-                    <TableCell data = {row[cell]} keyName = {allTablesFields[index][0]} rowID = {row.id} colID = {allTablesFields[index][colIndex]} tableName = {table.table_name}></TableCell>
-                   
+                    // <td><input value = {row[cell]} onInput={(event)=> {submitQuery(table.table_name, allTablesFields[index][colIndex], event.target.value, allTablesFields[index][0], row.id)}}></input></td>
+                    <TableCell URI = {URI} data = {row[cell]} keyName = {allTablesFields[index][0]} rowID = {row.id} colID = {allTablesFields[index][colIndex]} tableName = {table.table_name} ></TableCell>
+                  
                   ))}
                   {/* {console.log("ROWID", row.id, "COLID", allTablesFields[index], "TABLE", table.table_name)} */}
                 </tr>
@@ -90,6 +94,7 @@ const Display = async (props) =>{
   );
   };
   
+  
   export default Display;
 
   
@@ -98,3 +103,11 @@ const Display = async (props) =>{
       //LOOP 1: TABLE NAME 
       //LOOP 2: FIELDS
       //LOOP 2: TABLE DATA
+
+        // const submitQuery = (tableName, colID, newVal, keyName, rowID) => {
+  //   const updateQuery = `UPDATE ${tableName} SET ${colID} = ${newVal} WHERE ${keyName} = ${rowID} `
+  //   //const updateQuery = `UPDATE ${props.table_name} SET ${props.colID} = ___ WHERE ${props.keyName} = ${props.rowID} `
+  //   return client.query(updateQuery)
+  //}
+  //const [value, setValue] = useState("")
+  //await client.release() 
--- src/app/components/home/tableCell.tsx
+++ src/app/components/home/tableCell.tsx
@@ -2,19 +2,34 @@
 
 import React, { useState, useEffect } from "react";
 import Chart from "chart.js";
-import {Pool} from 'pg';
-import {Client} from '@elephantsql/client';
+import {Pool, Client} from 'pg';
+import { useServerEffect } from 'next/server';
 
 import "./style.css"
-
+import {executeQuery} from "./db"
 
 function TableCell (props) {
     
     const [value, setValue] = useState(`${props.data}`);
+//const updateQuery = `UPDATE ${props.table_name} SET ${props.colID} = ___ WHERE ${props.keyName} = ${props.rowID} `
+    console.log(props.URI)
+    // const pool = new Pool({
+    //     connectionString: props.URI
+    // })
+    // pool.end()
 
-    useEffect(() => {
+    function submitQuery(tableName, colID, newVal, keyName, rowID){
+        // const pool = new Pool({
+        //     connectionString: props.URI
+        // })
+        //let client2 = new Client(props.URI)
         
-    })
+        const updateQuery = `UPDATE ${tableName} SET ${colID} = ${newVal} WHERE ${keyName} = ${rowID} `
+        //executeQuery(updateQuery)
+        //const updateQuery = `UPDATE ${props.table_name} SET ${props.colID} = ${value} WHERE ${props.keyName} = ${props.rowID} `
+        //client.query(updateQuery)
+        //client.end()
+    }
     console.log("KEYNAME", props.keyName, "ROWID", props.rowID, "COLID", props.colID, "TABLE", props.tableName)
     return (
        
