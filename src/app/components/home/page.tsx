e5959168b95e6fbc5e7ff1d2113a2536a01773ad
mushfiq-khandaker <127776658+mushfiq-khandaker@users.noreply.github.com>
2023-05-24 14:14:08 -0400

    Trying to Connect Client to database

--- src/app/components/home/page.tsx
+++ src/app/components/home/page.tsx
@@ -4,7 +4,7 @@ import Display from "./display"
 
 function App() {
     const uri = "postgres://mmethhdd:OuENml3Y4wNyMcCHb69l16Cn3l2osxzh@drona.db.elephantsql.com/mmethhdd";
-  
+    
     return (
       <div>
         <h1>WELCOME TO THE HOME</h1>
