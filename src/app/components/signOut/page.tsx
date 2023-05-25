46196685b3e4e7620892481cb1d33c8fe36eb2cd
kennyyang2015 <kennyyang2015@gmail.com>
2023-05-25 13:22:41 -0400

    merging branch 'mush-kenny2' into devkenny / working oauth in devkenny and initial page

---
+++ src/app/components/signOut/page.tsx
@@ -0,0 +1,16 @@
+//when there is layout.tsx, next.js goes to layout to render rather than just the page
+
+'use client'
+import Link from 'next/link';
+import {signOut} from 'next-auth/react';
+
+export default function Something(){
+    return (
+        <div className='flex justify-center'>
+          <Link href="/" className='flex-initial'>
+            <button onClick={() => signOut({ callbackUrl: 'http://localhost:3000'})} className='text-lg' >Sign Out</button>
+          </Link> 
+        </div>
+      )
+};
+
