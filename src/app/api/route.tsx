a6539a27038b998131448e6f6c563155f7b2c1e1
kennyyang2015 <kennyyang2015@gmail.com>
2023-06-01 10:13:06 -0400

    completed routing form home to display

--- src/app/api/route.tsx
+++ src/app/api/route.tsx
@@ -10,23 +10,44 @@
 // }
 import { NextResponse } from 'next/server';
 //const URI = "postgres://mmethhdd:OuENml3Y4wNyMcCHb69l16Cn3l2osxzh@drona.db.elephantsql.com/mmethhdd";
-let pg = require('pg')
-const URI = "postgres://jztpyupa:iQAwglVEeRo17CrNnmg3IEKQstbLb-bu@jelani.db.elephantsql.com/jztpyupa";
-let client = new pg.Client(URI)
-//let updateQuery = `UPDATE ${request.body.tableName} SET ${colID} = ${newVal} WHERE ${keyName} = ${rowID} `
-client.connect()
 
+// let pg = require('pg')
+// const URI = "postgres://jxbiwedv:tWMx8_U1YtUH3Noj4vFCNMVW1yHOfEWb@jelani.db.elephantsql.com/jxbiwedv";
+// let client = new pg.Client(URI)
+// //let updateQuery = `UPDATE ${request.body.tableName} SET ${colID} = ${newVal} WHERE ${keyName} = ${rowID} `
+// client.connect()
 export async function PATCH(request: Request) {
-      //const {tableName, colID, newVal, keyName, rowID} = request.body
-      // console.log("THE BODY", await request.json())
-      // let query = await request.json()
-      // let pg = require('pg')
-      // const URI = "postgres://mmethhdd:OuENml3Y4wNyMcCHb69l16Cn3l2osxzh@drona.db.elephantsql.com/mmethhdd";
-      // let client = new pg.Client(URI)
-      // //let updateQuery = `UPDATE ${request.body.tableName} SET ${colID} = ${newVal} WHERE ${keyName} = ${rowID} `
-      // await client.connect()
-
-      return await client.query(await request.json())
+  //const {tableName, colID, newVal, keyName, rowID} = request.body
+  // console.log("THE BODY", await request.json())
+  // let query = await request.json()
+  // let pg = require('pg')
+  // const URI = "postgres://mmethhdd:OuENml3Y4wNyMcCHb69l16Cn3l2osxzh@drona.db.elephantsql.com/mmethhdd";
+  // let client = new pg.Client(URI)
+  // //let updateQuery = `UPDATE ${request.body.tableName} SET ${colID} = ${newVal} WHERE ${keyName} = ${rowID} `
+  // await client.connect()
+  
+  let pg = require('pg')
+  // const URI = "postgres://jxbiwedv:tWMx8_U1YtUH3Noj4vFCNMVW1yHOfEWb@jelani.db.elephantsql.com/jxbiwedv";
+  let client = new pg.Client(URI)
+  //let updateQuery = `UPDATE ${request.body.tableName} SET ${colID} = ${newVal} WHERE ${keyName} = ${rowID} `
+  client.connect()
+  return await client.query(await request.json())
     
     //   return NextResponse.json({ data });
+}
+
+let globalURI;
+let client
+export const POST = async(req,res) => {
+  const { URI } = await req.json()
+  console.log("URI=======", URI)
+  let pg = require('pg')
+  globalURI = URI
+  client = new pg.Client(globalURI)
+  client.connect()
+  return new Response(JSON.stringify(true))
+}
+
+export const GET = async(req, res) => {
+  return await client.query("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public';")
 }
---
+++ src/app/components/home/input.tsx
@@ -0,0 +1,45 @@
+'use client'
+import React, { useState,useRef }from 'react'
+import Display from "../display/display"
+import Link from 'next/link';
+import { useRouter } from "next/navigation";
+type Props = {}
+// postgres://jxbiwedv:tWMx8_U1YtUH3Noj4vFCNMVW1yHOfEWb@jelani.db.elephantsql.com/jxbiwedv
+const Input = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const [value, setValue] = useState('')
+  const [showComponent, setShowComponent] = useState(false);
+  const router = useRouter();
+  // const navigate = useNavigate()
+  const handleURI = async(value, e) => {
+    e.preventDefault();
+    const response = await fetch('/api/', {
+      method: 'POST',
+      body: JSON.stringify({
+        URI: value
+      })
+    })
+    .then(data => data.json())
+    .then(data => setShowComponent(data))
+    
+  }
+  if (!showComponent){
+    return (
+      <div>
+         <p>URI :</p>
+        <input type="text" placeholder='URI' value={value} onChange={(event)=>setValue(event.target.value)}>
+        </input>
+  
+          <button onClick={(e)=> handleURI(value, e)}>SUBMIT</button>
+
+      </div>
+    )
+  } else{
+    router.push("/components/display")
+  }
+}
+
+export default Input
