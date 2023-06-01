fe7041de25d51ae70d7bcdfe2ab15a5a858abceb
kennyyang2015 <kennyyang2015@gmail.com>
2023-06-01 10:13:06 -0400

    completed routing form home to display

---
+++ src/app/components/display/page.tsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+import Display from './display'
+type Props = {}
+
+const App = (props: Props) => {
+  return (
+    <div>
+      <Display />
+    </div>
+  )
+}
+
+export default App
--- src/app/components/home/page.tsx
+++ src/app/components/home/page.tsx
@@ -1,14 +1,17 @@
-import React from "react"
+
 //import ReactDOM from 'react-dom';
-import Display from "./display"
+import Display from "../display/display"
+import Input from "./input"
 
 function App() {
-    const uri = "postgres://jztpyupa:iQAwglVEeRo17CrNnmg3IEKQstbLb-bu@jelani.db.elephantsql.com/jztpyupa";
-    
+    const uri = "postgres://jxbiwedv:tWMx8_U1YtUH3Noj4vFCNMVW1yHOfEWb@jelani.db.elephantsql.com/jxbiwedv";
+ 
     return (
       <div>
-        <h1>WELCOME TO THE HOME</h1>
-        <Display URI = {uri}/>
+        <h1>PSQL</h1>
+        <Input/>
+        {/* <Display URI={uri}/> */}
+      
       </div>
     );
   };
@@ -17,3 +20,6 @@ function App() {
 
 
 
+  // <Input>
+  //         <Display URI={uri}/>
+  //       </ Input>
