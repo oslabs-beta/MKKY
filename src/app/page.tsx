e02083754bb75ca9fead826df7edccc87264af4e
Yohan Yang <yangyohan1@gmail.com>
2023-06-06 14:49:12 -0400

    cloud provider logos are symmetrical

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,17 +15,17 @@ const Welcome: React.FC = () => {
       <div className='carosel-layout'>
         <h1 className='text-4xl' id='carosel-heading' >Compatible Cloud Providers:</h1>
         <div className = 'row4'>
-          <div className = 'column4'>
-            <img className = 'carosel' src = 'https://cdn.gruntwork.io/gruntwork-website/24f52ffe0ccbd746c2103814e50fe009.png'></img>
+          <div className = 'column4' id='divimg1'>
+            <img className='carosel' id='img1'src='https://cdn.gruntwork.io/gruntwork-website/24f52ffe0ccbd746c2103814e50fe009.png'></img>
           </div>
-          <div className = 'column4'>
-            <img className = 'carosel' src = 'https://www.freecodecamp.org/news/content/images/2020/10/gcp.png'></img>
+          <div className='column4' id='divimg2'>
+            <img className='carosel' id='img2' src='https://www.freecodecamp.org/news/content/images/2020/10/gcp.png'></img>
           </div>
-          <div className = 'column4'>
-            <img className = 'carosel' src = 'https://www.imagar.com/wp-content/uploads/2018/06/azure.png'></img>
+          <div className='column4' id='divimg3'>
+            <img className='carosel' id='img3' src='https://www.imagar.com/wp-content/uploads/2018/06/azure.png'></img>
           </div>
-          <div className = 'column4'>
-            <img className = 'carosel' src = 'https://www.digitalocean.com/_next/static/media/do-opensource.b0d0088f.svg'></img>
+          <div className='column4' id='divimg4'>
+            <img className='carosel' id='img4' src='https://www.digitalocean.com/_next/static/media/do-opensource.b0d0088f.svg'></img>
           </div>
         </div>
       </div>
