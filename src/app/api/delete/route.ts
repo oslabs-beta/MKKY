25d02456855296800fbc515ecab7ca2388be0f42
mushfiq-khandaker <127776658+mushfiq-khandaker@users.noreply.github.com>
2023-06-08 10:27:49 -0400

    fixed the delete function

--- src/app/api/delete/route.ts
+++ src/app/api/delete/route.ts
@@ -4,11 +4,6 @@ export const POST = async(req) => {
     // const { uri, query, value } = req.json();
     const body = await req.json();
 
-
-    console.log('THE QUERY ======', body.query)
-    console.log('THE URI ======', body.uri)
-    console.log('THE VALUE ======', body.value)
-    // console.log('THE VALUE ======', body.value)
     const URI = body.uri;
     const query = body.query;
     let pg = require('pg')
